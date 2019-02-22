a26a7cce497cf949947daac43455355884ca5673
Nick R <nbruim33@hotmail.com>
2019-02-22 00:02:04 -0500

    Created Fanvest.Web project.

---
+++ Fanvest.Web/Controllers/HomeController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace Fanvest.Web.Controllers
+{
+    public partial class HomeController : Controller
+    {
+        public virtual IActionResult Index() => View();
+    }
+}
